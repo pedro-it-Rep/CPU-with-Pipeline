9b31d10efdfd11efd97a0a446500a3f2e556c88e
Cesar Marrote Manzano <43895268+cesarmmanzano@users.noreply.github.com>
2019-11-07 15:25:56 -0300

    Add files via upload

---
+++ Projeto CPU com Pipeline/Códigos/IF_ID.vhd
@@ -0,0 +1,30 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+entity IF_ID is
+	port(clock: in	std_logic;
+	
+		-- Não há sinais de controle nessa etapa
+		
+		-- 32 bits para o PC
+		pcIn: in std_logic_vector(0 to 31);
+		pcOut: out std_logic_vector(0 to 31) := "00000000000000000000000000000000";
+			
+		-- 32 bits para a instrução
+		InstructionIn:	in std_logic_vector(0 to 31);
+		InstructionOut:	out std_logic_vector(0 to 31) := "00000000000000000000000000000000");
+		
+end IF_ID;
+
+architecture IFID of IF_ID is
+begin
+	process(clock)
+		begin
+			if (clock'EVENT and clock = '1') then
+				out_instr <= in_instr;
+				out_pc <= in_pc;
+			end if;
+	end process;
+end IFID;
---
+++ Projeto CPU com Pipeline/Códigos/MEM_WB.vhd
@@ -0,0 +1,39 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+entity MEM_WB is
+	port (clock: in	std_logic;
+	
+			-- 32 bits para o sinal de WB
+			wbIn: in std_logic_vector(0 to 1);
+			wbOut: out	std_logic_vector(0 to 1) := "00";
+			
+			-- 32 bits para o  
+			rdIn: in std_logic_vector(0 to 31);
+			rdOut: out	std_logic_vector(0 to 31) := "00000000000000000000000000000000";
+			
+			-- 32 bits para o  
+			addrIn: in	std_logic_vector(0 to 31);
+			addrOut: out std_logic_vector(0 to 31) := "00000000000000000000000000000000";
+			
+			-- 4 bits para o  
+			redDstIn: in std_logic_vector(0 to 4);
+			redDstOut: out	std_logic_vector(0 to 4) := "00000");
+			
+end MEM_WB;
+
+architecture MEMWB of MEM_WB is
+
+begin
+	process(clock)
+	begin
+		if (clock'EVENT and clock = '1') then
+			wbOut 	<= wbIn;
+			rdOut <= rdIn;
+			addrOut <= addrIn;
+			redDstOut <= redDstIn;
+		end if;
+	end process;
+end MEMWB;
