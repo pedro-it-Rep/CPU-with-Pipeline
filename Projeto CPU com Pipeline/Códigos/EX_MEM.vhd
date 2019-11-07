ec6ab16aee1c49bf77b044b12e0455f77b2c3ec3
Cesar Marrote Manzano <43895268+cesarmmanzano@users.noreply.github.com>
2019-11-07 19:35:50 -0300

    Add files via upload

--- Projeto CPU com Pipeline/Códigos/EX_MEM.vhd
+++ Projeto CPU com Pipeline/Códigos/EX_MEM.vhd
@@ -3,35 +3,40 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 
-entity pipelineRegEXMEM is
-	port (clock:		in		std_logic;
+entity EX_MEM is
+
+	port (
+			clock: in std_logic;
+			
 			-- Declaração da Entrada e Saida do WB e ME
-			entradaWB:		in		std_logic_vector(0 to 1);
-			entradaME:		in		std_logic_vector(0 to 2);
-			saidaWB:		out	std_logic_vector(0 to 1) := "00";
-			saidaME:		out	std_logic_vector(0 to 2) := "000";
+			entradaWB: in std_logic_vector(0 to 1);
+			entradaME: in std_logic_vector(0 to 2);
+			saidaWB: out std_logic_vector(0 to 1) := "00";
+			saidaME: out std_logic_vector(0 to 2) := "000";
 			
 			-- Entrada e Saido do PC, responavel pela instrução/proxima instrução
-			entradaPC:		in		std_logic_vector(0 to 31);
-			saidaPC:		out	std_logic_vector(0 to 31) := "00000000000000000000000000000000";
+			entradaPC: in std_logic_vector(0 to 31);
+			saidaPC: out std_logic_vector(0 to 31) := "00000000000000000000000000000000";
 			
 			-- Entrada e Saido do sinal ZERO da ULA
-			entradaZERO:		in		std_logic;
-			saidaZERO:	out	std_logic := '0';
+			entradaZERO: in std_logic;
+			saidaZERO: out std_logic := '0';
 			
 			-- Entrada e saido do resultado da operação da ALU/ULA 
-			entradaResultado:	in		std_logic_vector(0 to 31);
+			entradaResultado: in std_logic_vector(0 to 31);
 			saidaResultado:	out	std_logic_vector(0 to 31) := "00000000000000000000000000000000";
 			
 			-- Entrada e Saida dos Dados que estão no Write Register
-			entrada_DadoWriteRegister:	in		std_logic_vector(0 to 31);
-			saida_DadoWriteRegister:	out	std_logic_vector(0 to 31) := "00000000000000000000000000000000";
+			entrada_DadoWriteRegister: in std_logic_vector(0 to 31);
+			saida_DadoWriteRegister: out std_logic_vector(0 to 31) := "00000000000000000000000000000000";
 			
-			entrada_RegDST:	in		std_logic_vector(0 to 4);
-			saida_RegDST:	out	std_logic_vector(0 to 4) := "00000");
-end pipelineRegEXMEM;
+			entrada_RegDST:	in std_logic_vector(0 to 4);
+			saida_RegDST: out std_logic_vector(0 to 4) := "00000"
+			);
+			
+end EX_MEM;
 
-architecture a of pipelineRegEXMEM is
+architecture EXMEM of EX_MEM is
 
 begin
 	process(clock)
@@ -39,12 +44,12 @@ begin
 	begin
 		if (clock'EVENT and clock = '1') then
 			saidaWB	<= entradaWB;
-			saidaME	<= entradaME;
+			saidaMEM <= entradaMEM;
 			saidaPC <= entradaPC;
 			saidaZERO <= entradaZERO;
 			saidaResultado 	<= entradaResultado;
 			saida_DadoWriteRegister	<= entrada_DadoWriteRegister;
-			saida_RegDST 	<= entrada_RegDST;
+			saida_RegDST <= entrada_RegDST;
 		end if;
 	end process;
-end;
+end EXMEM;
